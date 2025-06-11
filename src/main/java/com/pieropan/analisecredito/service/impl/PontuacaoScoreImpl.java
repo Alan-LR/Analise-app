c2d8ba818d23c12d2b63338c61288b289cce64aa
AlanLR <alanlopes.r@gmail.com>
2025-06-11 10:32:27 -0300

    FEAT:gerenciando as classes do spring com @component para preencher a lista de CalculoPonto e ordenação da lista com @order

--- src/main/java/com/pieropan/analisecredito/service/impl/PontuacaoScoreImpl.java
+++ src/main/java/com/pieropan/analisecredito/service/impl/PontuacaoScoreImpl.java
@@ -2,9 +2,13 @@ package com.pieropan.analisecredito.service.impl;
 
 import com.pieropan.analisecredito.domain.Proposta;
 import com.pieropan.analisecredito.service.strategy.CalculoPonto;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
+@Order(2)
+@Component
 public class PontuacaoScoreImpl implements CalculoPonto {
     @Override
     public int calcular(Proposta proposta) {
